8c4e28cd9b06e910ec37df88de4b4a4088b401a9
Алексиев <kadoll555@gmail.com>
2021-05-07 00:05:41 +0300

    built the highscore system to be recorded in text file

--- highscores.py
+++ highscores.py
@@ -1,8 +1,28 @@
 # intended to be the text file to read and write high scores at some point
-high_scores = [
-    ("", 0),
-    ("", 0),
-    ("", 0),
-    ("", 0),
-    ("", 0),
-]
+class HighScores:
+
+    def __init__(self):
+        self.high_scores = []
+
+    def get_scores(self):
+        with open("high_scores.txt", mode="r") as file:
+            for _ in range(5):
+                file_line = file.readline()
+                entry = file_line.replace('"', '').replace(':', '').replace('\\n', '').\
+                    replace('(', '').replace(')', "").replace('\'', '').split(',')
+                highscore = (entry[0], int(entry[1]))
+
+                self.high_scores.append(highscore)
+
+        print(self.high_scores)
+        return self.high_scores
+
+    def record_scores(self, high_scores):
+        with open("high_scores.txt", mode="w") as file:
+            for _ in high_scores:
+                file.write(f"{_}\n")
+
+        print(high_scores)
+
+
+
--- score.py
+++ score.py
@@ -1,21 +1,22 @@
 from turtle import Turtle, Screen
-from highscores import high_scores
+from highscores import HighScores
 
 
 class ScoreBoard:
 
     def __init__(self):
+        self.scores = HighScores()
         self.player_score = 0
         self.text_turtle = Turtle()
         self.screen = Screen()
         # to be a read/write from a text file and allow high scores to transcend to other runs of the program
-        self.high_scores = high_scores
+        self.high_scores = self.scores.get_scores()
 
     def display_score(self):
         self.text_turtle.penup()
         self.text_turtle.hideturtle()
         self.text_turtle.goto(0, 280)
-        self.text_turtle.color("red")
+        self.text_turtle.color("green")
         self.text_turtle.write(f"Player score: {self.player_score}", move=False, align="center",
                                font=("Arial", 14, "normal"))
 
@@ -30,6 +31,7 @@ class ScoreBoard:
             player_name = self.screen.textinput("High score detected!",
                                                 "What name should we record the high score under?")
             self.order_high_scores(player_name)
+            self.scores.record_scores(self.high_scores)
         self.display_high_scores()
 
     # this is weird, could be written cleaner
@@ -44,13 +46,15 @@ class ScoreBoard:
 
     def display_high_scores(self):
         turtle_x = 0
-        turtle_y = 0
+        turtle_y = 70
         self.text_turtle.goto(turtle_x, turtle_y)
         self.text_turtle.write(arg=f" Game Over!\nFinal score: {self.player_score}", align="center",
                                font=("Arial", 16, "normal"))
         turtle_y -= 40
         self.text_turtle.goto(turtle_x, turtle_y)
         self.text_turtle.write(arg=f" HIGH SCORES!", align="center", font=("Arial", 20, "normal"))
+
+        turtle_y -= 20
         for hscore in self.high_scores:
             turtle_y -= 20
             self.text_turtle.goto(turtle_x, turtle_y)
